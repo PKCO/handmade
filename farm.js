77f8f748b0a26517085af27a3cf05daa78f90d74
Christena <kpulagal@gitam.in>
2024-02-19 19:48:04 +0530

    Add files via upload

---
+++ farm.js
@@ -0,0 +1,9 @@
+function submitFarmerDetails() {
+    alert("Farmer details submitted!");
+    document.getElementById('farmerForm').reset();
+}
+
+function submitProduceDetails() {
+    alert("Produce details submitted!");
+    document.getElementById('produceForm').reset();
+}
